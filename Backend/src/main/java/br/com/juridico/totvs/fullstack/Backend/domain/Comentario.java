01405ae1c37de5ab3e67d86b9d7468d8edb1417d
Clara <clararemotto@gmail.com>
2025-10-20 16:58:27 -0300

    Cria a entidade Comentario com campos autor, texto e dataCriacao

---
+++ Backend/src/main/java/br/com/juridico/totvs/fullstack/Backend/domain/Comentario.java
@@ -0,0 +1,26 @@
+package br.com.juridico.totvs.fullstack.Backend.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comentario")
+public class Comentario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    //nome do autor do comentario
+    private String autor;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    //comentario
+    private String textoComentario;
+
+    @Column(nullable = false)
+    //data e hora do comentario
+    private LocalDateTime dataCriacao;
+
+
+}
