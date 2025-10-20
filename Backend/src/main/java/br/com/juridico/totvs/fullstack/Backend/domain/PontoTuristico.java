fd72e753066c5beb3b818b46e0e247681c376a3f
Clara <clararemotto@gmail.com>
2025-10-20 14:44:24 -0300

    Criando getters e setters de PontoTuristico

--- Backend/src/main/java/br/com/juridico/totvs/fullstack/Backend/domain/PontoTuristico.java
+++ Backend/src/main/java/br/com/juridico/totvs/fullstack/Backend/domain/PontoTuristico.java
@@ -60,7 +60,53 @@ public class PontoTuristico {
         this.resumo = request.getResumo();
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public MelhorEstacao getMelhorEstacao() {
+        return melhorEstacao;
+    }
+
+    public void setMelhorEstacao(MelhorEstacao melhorEstacao){
+        this.melhorEstacao = melhorEstacao;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 
 
 
