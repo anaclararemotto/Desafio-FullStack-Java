9a9b426a9b3fcdb1c8c287edf9f58494c08ebab6
Clara <clararemotto@gmail.com>
2025-10-21 14:42:24 -0300

    Implementa botão de adicionar novo ponto turistico

--- frontend/src/app/pontos-turisticos/pontos-turisticos.component.ts
+++ frontend/src/app/pontos-turisticos/pontos-turisticos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PontoTuristico, PontoTuristicoService } from '../service/ponto-turistico.service';
+import { PoPageAction, PoTableColumn } from '@po-ui/ng-components';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pontos-turisticos',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PontosTuristicosComponent implements OnInit {
 
-  constructor() { }
+  //armazena os pontos retornados pelo backend
+  pontosTuristicos: PontoTuristico[] = [];
+  //Configuração das colunas da tabela com PO-UI
+  colunas: PoTableColumn[] = [];
+  //Acoes da pag que aparecem no cabeçalho do po-page
+  acoes: PoPageAction[] = [];
+
+  //realiza chamada HTTP e injeta o router de navegação de telas
+  constructor( private pontoTuristicoService : PontoTuristicoService, private router : Router) { }
 
   ngOnInit(): void {
+    this.setupAcoes();
+  }
+
+  //configura as açoes do cabeçalho da pag
+  private setupAcoes(){
+    this.acoes = [{
+      label: 'Novo ponto turistico',
+      //navega para o formulario
+      action: () => this.router.navigate(['/pontos-turisticos/new']),
+      icon: 'po-icon-plus'
+    }]
   }
 
 }
